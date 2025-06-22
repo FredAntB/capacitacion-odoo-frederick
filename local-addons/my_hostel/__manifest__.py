70e732bdc2e5632443369fddef2fe1a03133ee66
Frederick Antonio Balderrama Mendoza <antoniobalderramamendoza@gmail.com>
2025-06-22 00:45:30 -0400

    Modified gitignore to exclude configuration files, added my_hostel module with its icon

---
+++ local-addons/my_hostel/__manifest__.py
@@ -0,0 +1,17 @@
+{
+        'name': "Hostel Management",
+        'summary': "Manage Hostel easily",
+        'description': "Efficiently manage the entire residential facility in the school.",
+        'author': "Your name",
+        'website': "http://www.example.com",
+        'category': 'Uncategorized',
+        'version': '17.0.1.0.0',
+        'depends': ['base'],
+        'data': ['views/hostel.xml'],
+        'assets': {
+            'web.assets_backend': [
+                'web/static/src/xml/**/*',
+                ],
+            },
+        'demo': ['demo.xml'],
+}
